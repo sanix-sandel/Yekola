e67df4339326c9ddd92b3ff27472ce93bbddd8b1
sanix sandel <sanicksikani@gmail.com>
2020-05-06 18:28:01 +0300

    mixins

--- yekola/courses/views.py
+++ yekola/courses/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Course
 
-# Create your views here.
+class ManageCourseListView(ListView):
+    model=Course
+    template_name='courses/manage/course.html'
+
+    def get_queryset(self):
+        qs=super().get_queryset()
+        return qs.filter(owner=self.request.user)
+
+class OwnerMixin(object):
+    def get_queryset(self):
+        qs=super().get_queryset()
+        rerturn qs.filter(owner=self.request.user)
+
+class OwnerEditMixin(object):
+    def form_valid(self, form):
+        form.instance.owner=self.request.user
+        return super().form_valid(form)
+
+class OwnerCourseMixin(OwnerMixin):
+    model=Course
+    fields=['subject', 'title', 'slug', 'overview']
+    success_url=reverse_lazy('manage_course_list')
+
+class OwnerCourseEditMixin(OwnerCourseMixin, OwnerEditMixin):
+    template_name='courses/manage/course/list.html'
+
+class CourseCreateView(OwnerCourseEditMixin, CreateView):
+    pass
+
+class CourseUpdateView(OwnerCourseEditmixin, UpdateView):
+    pass
+
+class CourseDeleteView(OwnerCourseMixin, DeleteView):
+    template_name='courses/manage/course/delete.html'
